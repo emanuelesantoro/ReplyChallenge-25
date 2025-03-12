117148a8d4f6f0eba57221370c051aad63aef8fc
Andrea Sanguineti <a.sanguineti@reply.it>
2025-03-12 15:41:54 +0100

    level0

--- andrea/test.py
+++ andrea/test.py
@@ -1,12 +1,19 @@
 import os
 
+level = "1-thunberg"
 # Move to the directory where the script is located
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-with open("input.txt", "r") as file:
+with open(f"{level}.txt", "r") as file:
     content = file.read()
 
-# La fessa
+content = """
+0 1 5
+1 1 2
+2 1 2
+4 2 2 2
+5 1 2
+"""
 
-with open("output.txt", "w") as file:
+with open(f"{level[0]}.txt", "w") as file:
     file.write(content)
